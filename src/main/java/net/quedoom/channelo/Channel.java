967703a1b270a14c8ccce40420c761f64ac7d3ea
QueDoom <norton@totl.se>
2025-06-14 23:48:24 +0200

    pluh

---
+++ src/main/java/net/quedoom/channelo/Channel.java
@@ -0,0 +1,16 @@
+package net.quedoom.channelo;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Channel implements ModInitializer {
+	public static final String MOD_ID = "channelo";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
